030210330292e4b0b794329c2a8d706018f7c56e
20060306 <shokirjonovanoila3@gmail.com>
2023-05-01 18:52:46 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+import { createServer } from 'http';
+import Express from './lib/express.js';
+
+
+import adminController from './controllers/admin.controller.js'
+import categoriesController from './controllers/categories.controller.js';
+import subCategoriesController from './controllers/subCategories.controller.js';
+import productsController from './controllers/products.controller.js';
+
+const PORT = 5000;
+
+function httpServer(req, res) {
+  const app = new Express(req, res);
+
+  app.get('/admin', adminController.GET);
+  app.get('/categories', categoriesController.GET);
+  app.get('/subcategories', subCategoriesController.GET);
+  app.get('/products', productsController.GET);
+
+  app.post('/signin', adminController.POST);
+  app.post('/categories', categoriesController.POST);
+  app.post('/subcategories', subCategoriesController.POST);
+  app.post('/products', productsController.POST);
+
+
+  app.put('/categories', categoriesController.PUT);
+  app.put('/subcategories', subCategoriesController.PUT);
+  app.put('/products', productsController.PUT);
+
+  app.delete('/categories', categoriesController.DELETE);
+  app.delete('/subcategories', subCategoriesController.DELETE);
+  app.delete('/products', productsController.DELETE);
+}
+
+createServer(httpServer).listen(PORT, () => console.log(`server runing ${PORT}`));
